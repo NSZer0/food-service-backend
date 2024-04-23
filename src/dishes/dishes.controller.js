10b2d9fe5a4098e1ae1afd9b36b8e4c0991ccc0b
NSZer0 <bradatima@gmail.com>
2024-04-23 14:41:09 -0500

    Added request body data validation check function

--- src/dishes/dishes.controller.js
+++ src/dishes/dishes.controller.js
@@ -6,4 +6,26 @@ const dishes = require(path.resolve("src/data/dishes-data"));
 // Use this function to assign ID's when necessary
 const nextId = require("../utils/nextId");
 
+// Validation functions
+function requestDataHasProperty(propertyName) {
+  return function (req, res, next) {
+    // Get the body data from the request
+    const { data = {} } = req.body;
+    // Make sure the property exists and is not empty
+    if (data[propertyName]) {
+      // If property is 'price' make sure it's a number and greater than 0
+      if (propertyName === "price" && ((typeof price !== "number") || (price <= 0))) {
+        // Invalid price data, return an error
+        return next({ status: 400, message: "Dish must have a price that is an integer greater than 0" });
+      }
+
+      // Data is valid, go to the next function
+      return next();
+    }
+
+    // Invalid data, return an error
+    next({ status: 400, message: `Dish must include a ${propertyName}` });
+  };
+}
+
 // TODO: Implement the /dishes handlers needed to make the tests pass
